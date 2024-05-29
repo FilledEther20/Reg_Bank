dbf41355b133289b89eb4c636a1c8a7fb95da28f
Chaitanya Gairola <gairolachaitanya@gmail.com>
2024-05-30 00:09:02 +0530

    Update main.go

--- CLI_GO/code/main.go
+++ CLI_GO/code/main.go
@@ -1,19 +1,1 @@
-package main
 
-import (
-	"fmt"
-	"time"
-)
-
-type item struct {
-	Task       string    //description of task
-	Status     bool      //Status of task ie done or not
-	CreatedAt  time.Time //time of creation
-	FinishedAt time.Time //time of completion
-}
-
-//now to store all the tasks we have to
-
-func main() {
-	fmt.Println("Hi")
-}
