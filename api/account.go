6924c46729e78e54bfe6639221f8c904901f5ca9
Chaitanya Gairola <gairolachaitanya@gmail.com>
2025-03-31 12:39:20 +0530

    sqlc yaml updated along with handler for listAccount defined

--- api/account.go
+++ api/account.go
@@ -34,6 +34,7 @@ func (server *Server) createAccount(ctx *gin.Context) {
 	ctx.JSON(http.StatusCreated, acc)
 }
 
+// Get account using ID.
 type getAccountRequest struct {
 	ID int64 `uri:"id" binding:"required,min=1"`
 }
@@ -58,5 +59,29 @@ func (server *Server) getAccount(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, acc)
 }
 
+// Lists account abiding pagination.
+type listAccountRequest struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
 
+func (server *Server) listAccount(ctx *gin.Context) {
+	var newRequest listAccountRequest
 
+	if err := ctx.ShouldBindUri(&newRequest); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := sqlc.ListAccountsParams{
+		Limit:  newRequest.PageSize,
+		Offset: (newRequest.PageID - 1) * newRequest.PageSize,
+	}
+	accounts, err := server.store.ListAccounts(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, accounts)
+}
